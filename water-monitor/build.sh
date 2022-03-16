66f92dd9204c2d1675c8bce82f66697a709adcbe
Marty Sweet <marty.sweet@omnistratus.co.uk>
2022-03-16 17:01:43 +0000

    cross arch docker build

--- water-monitor/build.sh
+++ water-monitor/build.sh
@@ -3,6 +3,13 @@
 ## TODO: Implement CI
 
 #docker login lhr.ocir.io
-docker build . -t water-monitor
-docker tag water-monitor:latest lhr.ocir.io/lrapvn2kw2t6/hem/water-monitor:1.0
-docker push lhr.ocir.io/lrapvn2kw2t6/hem/water-monitor:1.0
+
+docker buildx create --name armbuilder --use
+docker buildx inspect --bootstrap
+
+
+docker buildx build --platform linux/amd64,linux/arm64 -t lhr.ocir.io/lrapvn2kw2t6/hem/water-monitor:latest --push .
+#docker build . -t water-monitor
+#docker tag water-monitor:latest lhr.ocir.io/lrapvn2kw2t6/hem/water-monitor:1.0
+#docker push lhr.ocir.io/lrapvn2kw2t6/hem/water-monitor:1.0
+#docker run --privileged --rm tonistiigi/binfmt --install arm64,riscv64,arm
